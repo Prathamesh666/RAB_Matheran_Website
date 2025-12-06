1151ff8b2a0515894d3c949c4a1b46af9d1f6607
Prathamesh666 <prathameshbhurke666@gmail.com>
2025-12-06 11:47:44 +0530

    RAB Website Files

--- config.py
+++ config.py
@@ -1,26 +1,22 @@
-import os
-import secrets
+from dotenv import load_dotenv
+import os, secrets
+load_dotenv()
 
-# Replace with environment variables in production
-MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://PrathameshBhurke:Prathameshbhurke666@cluster0.ozajm.mongodb.net/hotel_db?retryWrites=true&w=majority&appName=Cluster0")
-SECRET_KEY = os.environ.get("SECRET_KEY", secrets.token_hex(16))
+MONGO_URI = os.getenv("MONGO_URI")
+SECRET_KEY = os.getenv("SECRET_KEY", secrets.token_hex(32))
 
-# SMTP optional settings for booking confirmation
-SMTP_HOST = os.environ.get("SMTP_HOST", "smtp.gmail.com")
-SMTP_PORT = int(os.environ.get("SMTP_PORT", "587"))
-SMTP_USER = os.environ.get("SMTP_USER", "p397366@gmail.com")
-SMTP_PASS = os.environ.get("SMTP_PASS", "abqh znwb ocgu qotn")
+SMTP_HOST = os.getenv("SMTP_HOST", "smtp.gmail.com")
+SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
+SMTP_USER = os.getenv("SMTP_USER")
+SMTP_PASS = os.getenv("SMTP_PASS")
 
-#Twilio optional settings for SMS notifications
-TWILIO_SID = os.environ.get("TWILIO_SID", "ACb5b86c7b344d1e068224ccd8621eb798")
-TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN", "31e6a407e10f8ec68895c6e018357239")
-TWILIO_PHONE = os.environ.get("TWILIO_PHONE", "+19302075295")
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_PHONE = os.getenv("TWILIO_PHONE")
 
-# Admin initial username (create via create_admin.py instead)
-ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
-ADMIN_EMAIL = os.environ.get("ADMIN_EMAIL", "prathameshbhurke666@gmail.com")
-ADMIN_PHONE = os.environ.get("ADMIN_PHONE", "+918433965801")
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "admin")
+ADMIN_EMAIL = os.getenv("ADMIN_EMAIL")
+ADMIN_PHONE = os.getenv("ADMIN_PHONE")
 
-#URL settings
-SERVER_NAME = "ranchoddasbhavan.com"   # or your deployed domain
-PREFERRED_URL_SCHEME = "https"         # ensures https links
+SERVER_NAME = os.getenv("SERVER_NAME", "ranchoddasbhavan.com")
+PREFERRED_URL_SCHEME = os.getenv("PREFERRED_URL_SCHEME", "https")
